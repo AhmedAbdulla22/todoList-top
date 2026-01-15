c4a08c5ec15124eaf1391ac7905a03b1b0ac025e
AhmedAbdulla22 <ahmeddev452@example.com>
2026-01-15 17:45:19 +0300

    feat: update TodoList to include sorting functionality and remove unused category list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import { format, toDate } from "date-fns";
 import { TodoList } from "./todo-list";
 
-<<<<<<< HEAD
 const list1 = TodoList("study");
-=======
-const list1 = TodoList();
-const list2 = TodoList("study");
->>>>>>> 63eab97a2b81863e510a652a83d5f0d0036dcd2e
 
 //add todos
 list1.addTodo({title: "complete Todo functionality",description: "well make it functional blah blah blah",dueDate: new Date()});
@@ -23,11 +18,6 @@ const list = list1.getTodoList();
 
 //show list arr
 console.log("list: ",list1.getTodoList());
-<<<<<<< HEAD
 console.log("sorted asc: ",list1.getSortedListAsc());
 console.log("sorted desc: ",list1.getSortedListDesc());
 console.log("category: ",list1.getCategory());
-=======
-console.log("category: ",list1.getCategory());
-console.log("category: ",list2.getCategory());
->>>>>>> 63eab97a2b81863e510a652a83d5f0d0036dcd2e
